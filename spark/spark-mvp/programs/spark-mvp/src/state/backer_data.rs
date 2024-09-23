a28aa73f7d2e50f9354dded4e66d71ae2b585251
Memewtoo <myuutuh@gmail.com>
2024-09-23 14:56:05 +0800

    Feat: Added backer_data_bumps field to store BackerData bumps

--- spark/spark-mvp/programs/spark-mvp/src/state/backer_data.rs
+++ spark/spark-mvp/programs/spark-mvp/src/state/backer_data.rs
@@ -4,11 +4,13 @@ use anchor_lang::prelude::*;
 pub struct BackerData {
     pub backer_pk: Pubkey,
     pub backer_amount: u64,
+    pub backer_data_bump: u8,
 }
 
 impl Space for BackerData {
     const INIT_SPACE: usize = 
         8 + // ACCOUNT DISCRIMINATOR
         32 + // BACKER PK
-        8; // BACKER AMOUNT
+        8 + // BACKER AMOUNT
+        1; // BACKER BUMP
 }
